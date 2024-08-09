16c9d52dc787102e91a52a074c963cfda863bbde
Adityarj1910 <adityarj1910@gmail.com>
2024-08-09 18:22:55 +0530

    js25

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    headers: {
-      'Permissions-Policy': 'interest-cohort=()',
-    },
-  },
+  // server: {
+  //   headers: {
+  //     'Permissions-Policy': 'interest-cohort=()',
+  //   },
+  // },
   base: '/portfolio/',
 })
   
