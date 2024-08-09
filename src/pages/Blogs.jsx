9232889c9aa415228318d8fe42b179ff81531e80
Adityarj1910 <adityarj1910@gmail.com>
2024-08-09 18:10:35 +0530

    js22

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -4,16 +4,16 @@ import Footer from '../components/Footer';
 import '../css/Blogs.css';
 
 // Import the card data and generateCard function from blog.js
-import { cards as cardData, generateCard } from '../assets/blog.js';
+// import { cards as cardData, generateCard } from '../assets/blog.js';
 
 function Blogs() {
   const [cards, setCards] = useState([]);
 
-  useEffect(() => {
-    // Generate cards using the generateCard function and set them in the state
-    const generatedCards = cardData.map(card => generateCard(card.title, card.text, card.time));
-    setCards(generatedCards);
-  }, []);
+  // useEffect(() => {
+  //   // Generate cards using the generateCard function and set them in the state
+  //   const generatedCards = cardData.map(card => generateCard(card.title, card.text, card.time));
+  //   setCards(generatedCards);
+  // }, []);
 
   return (
     <>
@@ -29,9 +29,9 @@ function Blogs() {
             {cards.map((card, index) => (
               <div key={index} className="blog">
                 <div className="card-body">
-                  <h3 className="card-title"><u>{card.title}</u></h3>
+                  {/* <h3 className="card-title"><u>{card.title}</u></h3>
                   <p className="card-text">{card.text}</p>
-                  <p className="card-time"><small className="text-body-secondary">{card.time}</small></p>
+                  <p className="card-time"><small className="text-body-secondary">{card.time}</small></p> */}
                 </div>
               </div>
             ))}
