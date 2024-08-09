11ff99c7757fcee08955feab4b7215945bd29ef1
Adityarj1910 <adityarj1910@gmail.com>
2024-08-09 19:02:57 +0530

    add blog.js

---
+++ src/assets/blog.js
@@ -0,0 +1,41 @@
+export const cards = [
+    {
+        title: 'Chat GPT and Open AI', 
+        text: 'This is a wider card with supporting text below as a natural lead-in to additional content.Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ratione nostrum vitae dolor vero numquam odit itaque harum nemo. Vitae nobis sunt, architecto ut tempora ipsum aut ad quaerat inventore molestiae quia neque earum error obcaecati commodi, fuga voluptate culpa.',
+        time: 'Last updated 3 mins ago'
+    },
+    {
+      title: 'Chat GPT and Open AI', 
+      text: 'This is a wider card with supporting text below as a natural lead-in to additional content  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error tempora nemo reprehenderit consectetur repellendus sint suscipit. Molestias, laborum! Alias magni accusamus asperiores unde error non nisi velit, molestias impedit, placeat quaerat architecto vel officiis magnam eos? Id omnis maxime voluptatibus enim? Ducimus beatae illo blanditiis, itaque, facere expedita saepe praesentium eaque et libero ipsa aliquid voluptates voluptatum, pariatur quae autem?.',
+      time: 'Last updated 3 mins ago'
+    },
+    {
+        title: 'Chat GPT and Open AI', 
+        text: 'This is a wider card with supporting text below as a natural lead-in to additional content.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut aliquid fugit eveniet voluptas totam ipsam reiciendis error. Recusandae cupiditate quisquam perspiciatis nobis natus veritatis labore quasi odit, ea in dignissimos aspernatur neque facilis omnis temporibus magnam delectus accusantium animi numquam quae consectetur officia qui sequi. Inventore amet voluptas, distinctio molestias ab quasi neque porro reprehenderit blanditiis aliquam corporis veritatis excepturi dolore dolores tempore labore facere reiciendis praesentium. Fugit, tempora et at explicabo unde accusamus fugiat, earum ut culpa aspernatur assumenda perferendis eveniet nesciunt accusantium quod molestias cupiditate sequi, nulla quidem reiciendis. Consequatur sint, omnis maxime repudiandae a tempore quos. Qui quidem blanditiis error cupiditate ex quisquam similique. Tempore qui omnis aliquid sint perspiciatis eaque suscipit similique culpa, illo mollitia illum alias maxime corrupti dolorem accusantium optio, neque impedit beatae error? Delectus architecto amet enim reiciendis repudiandae velit tenetur ducimus, eligendi consequatur quidem, iure officia laborum assumenda esse? Exercitationem placeat quae provident officiis animi dolore autem et! Saepe deserunt exercitationem rerum minus consequuntur aliquid labore quae id libero perspiciatis. Aut exercitationem aspernatur earum iste molestias voluptas nesciunt necessitatibus possimus vero, unde iure voluptatem animi provident sapiente molestiae harum nulla sit! Ipsam saepe nam quis maiores repudiandae ipsa sunt iusto, sint officia.',
+        time: 'Last updated 3 mins ago'
+    },
+    {
+        title: 'Chat GPT and Open AI', 
+        text: 'This is a wider card with supporting text below as a natural lead-in to additional content.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut aliquid fugit eveniet voluptas totam ipsam reiciendis error. Recusandae cupiditate quisquam perspiciatis nobis natus veritatis labore quasi odit, ea in dignissimos aspernatur neque facilis omnis temporibus magnam delectus accusantium animi numquam quae consectetur officia qui sequi. Inventore amet voluptas, distinctio molestias ab quasi neque porro reprehenderit blanditiis aliquam corporis veritatis excepturi dolore dolores tempore labore facere reiciendis praesentium. Fugit, tempora et at explicabo unde accusamus fugiat, earum ut culpa aspernatur assumenda perferendis eveniet nesciunt accusantium quod molestias cupiditate sequi, nulla quidem reiciendis. Consequatur sint, omnis maxime repudiandae a tempore quos. Qui quidem blanditiis error cupiditate ex quisquam similique. Tempore qui omnis aliquid sint perspiciatis eaque suscipit similique culpa, illo mollitia illum alias maxime corrupti dolorem accusantium optio, neque impedit beatae error? Delectus architecto amet enim reiciendis repudiandae velit tenetur ducimus, eligendi consequatur quidem, iure officia laborum assumenda esse? Exercitationem placeat quae provident officiis animi dolore autem et! Saepe deserunt exercitationem rerum minus consequuntur aliquid labore quae id libero perspiciatis. Aut exercitationem aspernatur earum iste molestias voluptas nesciunt necessitatibus possimus vero, unde iure voluptatem animi provident sapiente molestiae harum nulla sit! Ipsam saepe nam quis maiores repudiandae ipsa sunt iusto, sint officia.',
+        time: 'Last updated 3 mins ago'
+    },
+    {
+        title: 'Chat GPT and Open AI', 
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium exercitationem ipsa quaerat quo. Sunt beatae tempora blanditiis illum quis molestias eum adipisci soluta maiores voluptates dolores aut alias laboriosam animi placeat, numquam repudiandae sit! Illum, fugit itaque exercitationem, omnis maxime odio ea vel in temporibus molestias vitae dolorum facilis provident rem voluptatibus laboriosam. Unde animi ipsum error a, culpa et mollitia perferendis recusandae consectetur, quis fugiat adipisci, distinctio voluptate nemo quidem numquam magnam modi molestiae iure reprehenderit ab quae eos amet at. Aliquam, dolore ullam vel debitis voluptatibus ipsam nam.',
+        time: 'Last updated 3 mins ago'
+    },
+    {
+        title: 'Chat GPT and Open AI', 
+        text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate similique tempore optio fuga veritatis ipsum temporibus accusamus totam? Quidem repellat sit harum et commodi a aliquam eum dignissimos illo debitis itaque blanditiis quisquam aspernatur distinctio, labore rerum ipsa, tenetur in praesentium tempore, exercitationem voluptates mollitia quae voluptate? Ad commodi non at repellat dolore optio earum unde provident hic sit esse totam nisi doloremque illum sint sunt necessitatibus consequatur veritatis, expedita, quam corrupti. Odit accusamus nemo soluta culpa eius quo neque minus animi hic iste reiciendis, impedit perspiciatis amet voluptatum? Saepe tempora expedita, error, voluptas iste, culpa nostrum numquam id a totam rerum tenetur eveniet vel incidunt molestias. Et, adipisci dolor! Sequi eligendi ex tempora veritatis ab dignissimos molestiae neque soluta commodi tenetur suscipit consequuntur nam est eaque excepturi dolorem voluptate debitis consectetur repellendus non quibusdam voluptatum, odit voluptatem? Illum officiis doloremque fuga numquam nesciunt corrupti aperiam, enim iusto ipsum. Doloremque praesentium numquam perspiciatis itaque? Quia quaerat qui quisquam! Aspernatur esse magnam magni doloribus pariatur veritatis ipsum maxime, autem dolore reprehenderit eaque, dolor vero. Esse sed reprehenderit deserunt itaque id magnam laboriosam natus alias exercitationem aliquid error, libero, totam soluta dolorem autem sequi consequuntur quisquam delectus, similique nulla. Id pariatur quasi temporibus consectetur et ullam unde quae reiciendis nobis deleniti quos facere explicabo a libero voluptate aspernatur consequuntur excepturi culpa provident repellendus, earum facilis? Beatae ipsum at quos expedita ullam veniam culpa nulla eum rerum quam, aliquid debitis saepe distinctio quas officia velit. Enim, at optio fuga nobis quam amet non.',
+        time: 'Last updated 3 mins ago'
+    },
+];
+
+export const generateCard = (title, text, time) => {
+    return {
+        title,
+        text,
+        time
+    };
+};
+
